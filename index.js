7d5cb1f6915088acb516533ccaa89d99237997fe
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-06-14 16:20:57 -0700

    Q-A-Task-1 commit

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
-  2. Which of the two uses a closure? How can you tell?
-  
+     
+    counter1 is the function expression and counter2 is a function declaration.
+
+  2. Which of the two uses a closure? How can you tell? 
+
+    counter 1 uses closure as the counter function has an access to counterMaker function
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+     For long or complex functions counter1 code is preferable for better organization, in all simple functions we can use counter2
 */
 
 // counter1 code
